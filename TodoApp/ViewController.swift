ecb7d99c269d68c326fcdf0906f5f9a01010375e
Yangyifan Wang <ywang20@mail.sfsu.edu>
2020-04-03 19:30:52 -0700

    Revert "Revert "Initial commit - UI skeleteon""
    This reverts commit 8a9e029eda9ab8ca1a824fb00e67bb1ad0d53229.

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Kim Wang on 4/3/20.
+//  Copyright © 2020 Kim Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
